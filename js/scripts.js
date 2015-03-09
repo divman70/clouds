4fa85f9dc6adc3ebc7333cfdec6e1f559d0f9dce
divman70 <dkoch6@engr-134-17.eastern.ewu.edu>
2015-03-09 13:39:52 -0700

    quiz #2

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$('#sky').jQlouds({
+  minClouds: 3, // minimum amount of clouds
+  maxClouds: 10, // maximum amount of clouds
+});
+
+$('#sky').jQlouds({
+  maxWidth: 300, // max image's width
+  maxHeight: 200, // amx image's height
+});
